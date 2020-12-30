661a00bbabbdf6cfd402a6c60a3c861aca7dc2e8
ChanwooJeong <chanwooda0223@gmail.com>
2020-12-30 12:12:52 +0900

    Revert "수정"
    This reverts commit 79a7a27bc516b0ab875ff147adcefef653d497f2.

--- marker-anomaly-detector/model_lstmvae.py
+++ marker-anomaly-detector/model_lstmvae.py
@@ -48,6 +48,11 @@ class MetricPredictor:
 
         self.number_of_features = number_of_feature
         self.scalar = MinMaxScaler(feature_range=(-1, 1))
+#         self.model = None
+#         self.parameter_tuning = parameter_tuning
+#         self.validation_ratio = validation_ratio
+#         self.mean = None
+#         self.std = None
         self.table_name = table_name
         self.col_name = col_name
         self.raw_roll = np.zeros((1,self.number_of_features,1))
@@ -55,15 +60,30 @@ class MetricPredictor:
         self.z_dim = 10
         self.model_dir = model_dir
 
+#     def prepare_data(self, data):
+#         """Prepare the data for LSTM."""
+#         train_x = np.array(data[:, 1])[np.newaxis, :].T
+# #         print(train_x.shape)
+#         for i in range(self.number_of_features):
+#             train_x = np.concatenate((train_x, np.roll(data[:, 1], -i)[np.newaxis, :].T), axis=1)
+# #         print(train_x.shape)
+#         train_x = train_x[:train_x.shape[0] - self.number_of_features, :self.number_of_features]
+
+#         train_yt = np.roll(data[:, 1], -self.number_of_features + 1)
+#         train_y = np.roll(data[:, 1], -self.number_of_features)
+#         train_y = train_y - train_yt
+#         train_y = train_y[:train_y.shape[0] - self.number_of_features]
+
+#         train_x = train_x.reshape(train_x.shape[0], 1, train_x.shape[1])
+        
+#         return train_x, train_y
 
     def unroll_auto(self, data, sequence_length=24):
         result = []
         for index in range(len(data) - sequence_length + 1):
             result.append(data[index: index + sequence_length])
         return np.asarray(result)
-    
-    
-    # get base model
+
     def get_model(self, lstm_dim = 30, z_dim = 10, predict_=False):
         """Build the model."""
         
@@ -72,7 +92,6 @@ class MetricPredictor:
 
         return vae
     
-    # load model by weight
     def reload_model(self, initialize_load = True):
         if initialize_load:
             self.model_dir = os.getcwd()+'/'+self.model_dir + '/{table_name}_{col_name}/{model_name}'.format(table_name = self.table_name, col_name = self.col_name, model_name = self.model_name)
@@ -85,15 +104,16 @@ class MetricPredictor:
         scalar_file_name = self.model_dir + '/' + 'scalar.pkl'
         self.scalar = joblib.load(scalar_file_name)
         _LOGGER.info("RESTORING MODEL RETURN START")
+#         model.load_model(self.model_dir)
+#         _LOGGER.info("RESTORING MODEL RETURN")
+#         return model
     
-#     def get_variable(self):
-#         _LOGGER.info("RETRUN MODEL VARIABLE")
-#         variables = {"model_variable" : [self.x_dim, self.z_dim],
-#                     "model_savedir" : self.model_dir}
-#         return variables
-    
-    
-    # model train - 
+    def get_variable(self):
+        _LOGGER.info("RETRUN MODEL VARIABLE")
+        variables = {"model_variable" : [self.x_dim, self.z_dim],
+                    "model_savedir" : self.model_dir}
+        return variables
+
     def train(self, metric_data=None, prediction_duration=15):
         """Train the model."""
         
@@ -204,10 +224,12 @@ class MetricPredictor:
         
         dataframe_cols["yhat_upper"] = upper_bound
         dataframe_cols["yhat_lower"] = lower_bound
+#         _LOGGER.info("YHAT : {} UPPER : {}  LOWER : {}".format(dataframe_cols["yhat"], 
+#                                                                dataframe_cols["yhat_upper"],
+#                                                                dataframe_cols["yhat_lower"]
+#                                                               )
+#                     )
         
-    
-    
-    
         # prophet model(origin value base)
 #         anomaly = 1
 #         if (
@@ -236,3 +258,210 @@ class MetricPredictor:
         
         
         
+        
+        
+        
+        
+        
+#         forecast = pd.DataFrame(data = dataframe_cols)
+        
+#         print(dataframe_cols)
+        
+        
+        
+        
+        
+#         nearest_index = self.predicted_df.index.get_loc(
+#             prediction_datetime, method="nearest"
+#         )
+#         return dataframe_cols
+    
+            
+            
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         labels = np.zeros_like(values, dtype=np.int32)
+#         timestamps = pd.DatetimeIndex(timestamps).astype(np.int64)
+        
+        
+#         train_values, mean, std = standardize_kpi(
+#             train_values, excludes=np.logical_or(train_labels, train_missing))
+#         self.mean = mean
+#         self.std = std
+        
+        
+#         model_h_for_p_x, model_h_for_q_z = get_model()
+#         model_dounut = Donut(model_h_for_p_x, model_h_for_q_z, x_dims=self.x_dims, z_dims=self.z_dim)
+#         trainer = DonutTrainer(model = model_donut, max_epoch = 100)
+#         predictor = DonutPredictor(model_donut)
+
+        
+        
+        
+        
+        
+#         _LOGGER.info(
+#             "training data range: %s - %s", self.metric.start_time, self.metric.end_time
+#         )
+#         _LOGGER.debug("begin training")
+#         _LOGGER.debug(values.shape)
+        
+#         sess = tf.InteractiveSession()
+#         trainer.fit(values = train_values,
+#         labels = train_labels,
+#         missing = train_missing,
+#         mean = mean,
+#         std= std,
+#         valid_portion = self.validation_ratio            
+#                    )
+        
+#         model = predictor
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+       
+        
+        
+        
+        
+        
+#         t_idxs = pd.DatetimeIndex(abnormal.index).astype(np.int64)
+        
+        
+#         print(self.metric)
+#         print(self.metric.metric_values.values)
+#         print(pd.DatetimeIndex(self.metric.metric_values.ds).astype(np.int64))
+#         aa
+        
+        
+        
+        
+#         metric_values_np = self.metric.metric_values.values
+            
+            
+            
+    
+
+#         # normalising
+#         metric_values_np = self.metric.metric_values.values
+#         scaled_np_arr = self.scalar.fit_transform(metric_values_np[:, 1].reshape(-1, 1))
+#         metric_values_np[:, 1] = scaled_np_arr.flatten()
+
+#         if self.parameter_tuning:
+#             x, y = self.prepare_data(metric_values_np)
+#             lstm_cells = [2 ** i for i in range(5, 8)]
+#             dense_cells = [2 ** i for i in range(5, 8)]
+#             loss = np.inf
+#             lstm_cell_count = 0
+#             dense_cell_count = 0
+#             for lstm_cell_count_ in lstm_cells:
+#                 for dense_cell_count_ in dense_cells:
+#                     model = self.get_model(lstm_cell_count_, dense_cell_count_)
+#                     model.compile(loss='mean_squared_error', optimizer='adam')
+#                     history = model.fit(x, y, epochs=50, batch_size=512, verbose=0,
+#                                         validation_split=self.validation_ratio)
+#                     val_loss = history.history['val_loss']
+#                     loss_ = min(val_loss)
+#                     if loss > loss_:
+#                         lstm_cell_count = lstm_cell_count_
+#                         dense_cell_count = dense_cell_count_
+#                         loss = loss_
+#             self.lstm_cell_count = lstm_cell_count
+#             self.dense_cell_count = dense_cell_count
+#             self.parameter_tuning = False
+
+#         model = self.get_model(self.lstm_cell_count, self.dense_cell_count)
+#         _LOGGER.info(
+#             "training data range: %s - %s", self.metric.start_time, self.metric.end_time
+#         )
+#         # _LOGGER.info("training data end time: %s", self.metric.end_time)
+#         _LOGGER.debug("begin training")
+#         data_x, data_y = self.prepare_data(metric_values_np)
+#         _LOGGER.debug(data_x.shape)
+#         model.compile(loss='mean_squared_error', optimizer='adam')
+#         model.fit(data_x, data_y, epochs=50, batch_size=512)
+#         data_test = metric_values_np[-self.number_of_features:, 1]
+#         forecast_values = []
+#         prev_value = data_test[-1]
+#         for i in range(int(prediction_duration)):
+#             prediction = model.predict(data_test.reshape(1, 1, self.number_of_features)).flatten()[0]
+#             curr_pred_value = data_test[-1] + prediction
+#             scaled_final_value = self.scalar.inverse_transform(curr_pred_value.reshape(1, -1)).flatten()[0]
+#             forecast_values.append(scaled_final_value)
+#             data_test = np.roll(data_test, -1)
+#             data_test[-1] = curr_pred_value
+#             prev_value = data_test[-1]
+
+#         dataframe_cols = {"yhat": np.array(forecast_values)}
+
+#         upper_bound = np.array(
+#             [
+#                 (
+#                         forecast_values[i] + (np.std(forecast_values[:i]) * 2)
+#                 )
+#                 for i in range(len(forecast_values))
+#             ]
+#         )
+#         upper_bound[0] = np.mean(
+#             forecast_values[0]
+#         )  # to account for no std of a single value
+#         lower_bound = np.array(
+#             [
+#                 (
+#                         forecast_values[i] - (np.std(forecast_values[:i]) * 2)
+#                 )
+#                 for i in range(len(forecast_values))
+#             ]
+#         )
+#         lower_bound[0] = np.mean(
+#             forecast_values[0]
+#         )  # to account for no std of a single value
+#         dataframe_cols["yhat_upper"] = upper_bound
+#         dataframe_cols["yhat_lower"] = lower_bound
+
+#         data = self.metric.metric_values
+#         maximum_time = max(data["ds"])
+#         dataframe_cols["timestamp"] = pd.date_range(
+#             maximum_time, periods=len(forecast_values), freq="min"
+#         )
+
+#         forecast = pd.DataFrame(data=dataframe_cols)
+#         forecast = forecast.set_index("timestamp")
+
+#         self.predicted_df = forecast
+#         _LOGGER.debug(forecast)
+
+#     def predict_value(self, prediction_datetime):
+#         """Return the predicted value of the metric for the prediction_datetime."""
+#         nearest_index = self.predicted_df.index.get_loc(
+#             prediction_datetime, method="nearest"
+#         )
+#         return self.predicted_df.iloc[[nearest_index]]
